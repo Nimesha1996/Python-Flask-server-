981c1e550c7b4a96c3a97fcb3b696743e3d8cd1e
Nimesha1996 <nimesha.chathurangi19@gmai.com>
2020-12-19 15:45:24 +0530

    user delete working

--- openapi_server/controllers/user_controller.py
+++ openapi_server/controllers/user_controller.py
@@ -7,21 +7,7 @@ from openapi_server import util
 from openapi_server.appConfig import db 
 
 def create_user():  # noqa: E501
-    """Create user
-
-    This can only be done by the logged in user. # noqa: E501
-
-    :param user: Created user object
-    :type user: dict | bytes
-
-    :rtype: User
-    
-    if connexion.request.is_json:
-        user = User.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
-    """
-    
-   
+       
     id = request.json.get('id' , '')
     username = request.json.get('username', '')
     firstName= request.json.get('firstName', '')
@@ -39,18 +25,24 @@ def create_user():  # noqa: E501
     return user_schema.jsonify(new_user)
 
 def create_users_with_list_input(user=None):  # noqa: E501
-    """Creates list of users with given input array
 
-    Creates list of users with given input array # noqa: E501
+    id = request.json.get('id' , '')
+    username = request.json.get('username', '')
+    firstName= request.json.get('firstName', '')
+    lastName = request.json.get('lastName', '')
+    password = request.json.get('password', '')
+    email = request.json.get('email', '')
+    phone = request.json.get('phone','')
+    userStatus = request.json.get('userStatus', '')
 
-    :param user: 
-    :type user: list | bytes
+    new_user = User(id=id,username=username,firstName=firstName,lastName=lastName,password=password,email=email,phone=phone,userStatus=userStatus)
 
-    :rtype: User
-    """
-    if connexion.request.is_json:
-        user = [User.from_dict(d) for d in connexion.request.get_json()]  # noqa: E501
-    return 'do some magic!'
+    db.session.add(new_user)
+    db.session.commit()
+
+    return user_schema.jsonify(new_user)
+
+    
 
 
 def delete_user(username):  # noqa: E501
@@ -65,13 +57,15 @@ def delete_user(username):  # noqa: E501
     
     return 'do some magic!'
     """
+    
+    user = User.query.filter_by(username=username).first_or_404()
+  
+    print(username)
 
-    user = User.query.get(username)
-    print(user)
-    username.session.delete(user)
-    username.session.commit()
-
-    return username.jsonify(user)
+    db.session.delete(user)
+    db.session.commit()
+     
+    return user_schema.jsonify(user)
 
 def get_user_by_name(username):  # noqa: E501
     """Get user by user name
